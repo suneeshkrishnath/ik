c39a3cb398e9d01fa588d94068e21a12081532d6
Suneesh <suneesh0388@gmail.com>
2024-07-29 14:44:07 +0200

    a sample project  added

---
+++ sp-git-test/sp-git-test/src/main/java/com/ik/springboot/git/SpGitTestApplication.java
@@ -0,0 +1,13 @@
+package com.ik.springboot.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpGitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpGitTestApplication.class, args);
+	}
+
+}
---
+++ sp-git-test/sp-git-test/src/test/java/com/ik/springboot/git/SpGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ik.springboot.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
